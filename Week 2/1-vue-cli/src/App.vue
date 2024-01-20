a07d7f8d58f561c27b401c0724e5c28386efe388
Muhammet Sarıcan <muhammetsarican@gmail.com>
2024-01-20 22:57:08 +0300

    started to use cli.

---
+++ Week 2/1-vue-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <app-header />
+  <div class="container">
+    <h3>I am working on vue js...</h3>
+    <p>Da ye no whu I am</p>
+  </div>
+  <CounterItem />
+  <counter-item />
+</template>
+
+<script>
+import CounterItem from "@/components/CounterItem.vue";
+
+export default {
+  components: {
+    CounterItem: CounterItem,
+  }
+}
+</script>
+
+<style></style>
---
+++ Week 2/1-vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h3>Working vue with Kablosuz Kedi</h3>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+<style>
+header {
+    background: rebeccapurple;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    width: 100%;
+    height: 50px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    border-bottom: 10px solid #333;
+}
+</style>
