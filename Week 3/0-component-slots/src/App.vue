eaeaf42240e74c2b8e6b5ba3ad9efc0117501397
Muhammet Sarıcan <muhammetsarican@gmail.com>
2024-01-21 22:01:47 +0300

    slots added.

---
+++ Week 3/0-component-slots/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ModalComponent>
+    <template #title>
+      <h3>Title that coming with slot</h3>
+    </template>
+
+    <template v-slot:body>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis soluta animi error unde sit libero nemo molestias tempora. Officiis odit consequatur maiores placeat dicta, porro beatae rerum similique alias quidem fugiat blanditiis minus id cumque tempora, eum aspernatur quibusdam optio ullam. Veniam voluptatibus quae odio facilis quaerat deserunt modi vero.
+    </template>
+
+    <template #default>
+      This text shown as default.
+    </template>
+  </ModalComponent>
+</template>
+<script>
+import ModalComponent from './components/ModalComponent.vue';
+
+export default {
+  components: {
+    ModalComponent
+  }
+}
+</script>
---
+++ Week 3/0-component-slots/src/components/ModalComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <slot name="title"></slot>
+        <hr>
+        <p class="text-green my-2">
+            <slot name="body"/>
+
+        </p>
+        <p><b><slot /></b></p>
+        <hr>
+        <div class="d-flex justify-content-end">
+            <button>Kapat</button>
+            <button class="green">Kaydet</button>
+        </div>
+    </div>
+</template>
